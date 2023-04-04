386b78efaadfc50a8cf2f9b2a1fe4e64cffe094b
Jay Two <j2doll@gmail.com>
2023-04-04 18:06:05 +0900

    update text

--- MPAA.py
+++ MPAA.py
@@ -38,7 +38,8 @@ import numpy as np
 import matplotlib
 matplotlib.rc('font', family='Malgun Gothic',size=8, weight = 'bold')
 
-파일 = pd.ExcelFile('N:\투자시뮬\통합 포트엑셀1.xlsx')
+# 파일 = pd.ExcelFile('N:\투자시뮬\통합 포트엑셀1.xlsx')
+파일 = pd.ExcelFile(r'D:\workspace\github\MPAA\MPAA-data.xlsx')
 자산군 = 파일.sheet_names
 자산군
 
